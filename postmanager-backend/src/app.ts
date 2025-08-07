29508124f0d4d9515712259503fc4664f66c2d39
Вадим Бобров <71228131+LoggerNice@users.noreply.github.com>
2025-08-07 20:22:25 +0300

    v 2.0.1
    Исправлено:
    - переадресация на сервер

--- postmanager-backend/src/app.ts
+++ postmanager-backend/src/app.ts
@@ -22,7 +22,7 @@ const wsServer = new WebSocketServer(server);
 setWebSocketServer(wsServer);
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "http://172.17.118.89:3000", "http://172.17.118.89:3001"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
